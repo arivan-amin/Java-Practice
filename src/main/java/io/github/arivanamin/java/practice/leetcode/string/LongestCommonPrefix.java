ae69036bea202a3362a52c73e59bab931ccef3da
Arivan Amin <arivanamin@gmail.com>
2025-06-19 11:25:06 +0300

    Add longest common prefix exercise

--- src/main/java/io/github/arivanamin/java/practice/leetcode/string/LongestCommonPrefix.java
+++ src/main/java/io/github/arivanamin/java/practice/leetcode/string/LongestCommonPrefix.java
@@ -1,5 +1,43 @@
 package io.github.arivanamin.java.practice.leetcode.string;
 
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+@Slf4j
 public class LongestCommonPrefix {
 
+    public String findCommonPrefix (List<String> words) {
+
+        return IntStream.range(0, getMinLength(words))
+            .mapToObj(i -> extractCharactersAtIndex(words, i))
+            .filter(LongestCommonPrefix::didAllCharactersMatch)
+            .map(List::getFirst)
+            .collect(Collectors.joining());
+    }
+
+    private static int getMinLength (List<String> words) {
+        return words.stream()
+            .map(String::length)
+            .mapToInt(Integer::intValue)
+            .min()
+            .orElseThrow();
+    }
+
+    private static List<String> extractCharactersAtIndex (List<String> words, int index) {
+        List<String> chars = new ArrayList<>();
+        for (String word : words) {
+            chars.add(String.valueOf(word.charAt(index)));
+        }
+        return chars.stream()
+            .distinct()
+            .toList();
+    }
+
+    private static boolean didAllCharactersMatch (List<String> chars) {
+        return chars.size() == 1;
+    }
 }
--- src/test/java/io/github/arivanamin/java/practice/leetcode/string/LongestCommonPrefixTest.java
+++ src/test/java/io/github/arivanamin/java/practice/leetcode/string/LongestCommonPrefixTest.java
@@ -2,18 +2,51 @@ package io.github.arivanamin.java.practice.leetcode.string;
 
 import io.github.arivanamin.java.practice.BaseUnitTest;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @Slf4j
 class LongestCommonPrefixTest implements BaseUnitTest {
 
+    private LongestCommonPrefix commonPrefix;
+    private List<String> words;
+
+    @BeforeEach
+    void setUp () {
+        commonPrefix = new LongestCommonPrefix();
+        words = new ArrayList<>();
+    }
+
     @Test
-    void Should () {
+    void findCommonPrefixShouldReturnEmptyStringWhenNoCommonPrefixExists () {
         // given
+        words.add("alpha");
+        words.add("bravo");
+        words.add("delta");
 
         // when
+        String result = commonPrefix.findCommonPrefix(words);
 
         // then
+        assertThat(result).isEmpty();
+    }
 
+    @Test
+    void findCommonPrefixShouldReturnCorrectPrefix () {
+        // given
+        words.add("flower");
+        words.add("flow");
+        words.add("flight");
+
+        // when
+        String result = commonPrefix.findCommonPrefix(words);
+
+        // then
+        assertThat(result).isEqualTo("fl");
     }
 }
